b0022a4507d8460af092497d59153e578a445ea8
Xenthio <thegooster88@gmail.com>
2025-12-03 19:10:48 +1100

    stuff

--- gamemodes/campaign/gamemode/init.lua
+++ gamemodes/campaign/gamemode/init.lua
@@ -8,10 +8,33 @@ function GM:Initialize()
 	print("[Campaign] Campaign Gamemode Initialized")
 end
 
--- Player spawn - remove HEV suit (you don't start with it in HL2)
-function GM:PlayerSpawn(ply)
-	self.BaseClass:PlayerSpawn(ply)
+-- Player spawn - only remove suit if they haven't picked it up yet
+-- function GM:PlayerSpawn(ply)
+-- 	self.BaseClass:PlayerSpawn(ply)
 	
-	-- Remove the suit
-	ply:RemoveSuit()
-end
+-- 	-- Only remove suit if player hasn't picked it up yet
+-- 	if ply:GetPData("campaign_has_suit", "0") ~= "1" then
+-- 		ply:RemoveSuit()
+-- 	end
+	
+-- 	-- Check suit status shortly after spawn and save it
+-- 	timer.Simple(0.5, function()
+-- 		if IsValid(ply) and ply:IsSuitEquipped() then
+-- 			ply:SetPData("campaign_has_suit", "1")
+-- 		end
+-- 	end)
+-- end
+
+-- -- Save suit status when player disconnects or changes level
+-- function GM:PlayerDisconnected(ply)
+-- 	if ply:IsSuitEquipped() then
+-- 		ply:SetPData("campaign_has_suit", "1")
+-- 	end
+-- end
+
+-- -- Also save on death (in case of level transition)
+-- function GM:PostPlayerDeath(ply)
+-- 	if ply:IsSuitEquipped() then
+-- 		ply:SetPData("campaign_has_suit", "1")
+-- 	end
+-- end
